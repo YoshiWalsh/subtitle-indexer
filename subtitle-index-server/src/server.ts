677659a98fcdb00cd2c90dd73af9de808b489569
Joshua Walsh <josh@joshwalsh.me>
2022-06-11 00:17:47 +1000

    Update JSON payload size limit
    Previously it was not possible to export large amounts of subtitles. This caused problems with some advanced typesetting.

--- subtitle-index-server/src/server.ts
+++ subtitle-index-server/src/server.ts
@@ -4,7 +4,7 @@ import { default as cors } from 'cors';
 export const app = express();
 const outputDirectory = process.env.OUTPUT_DIRECTORY || "./data/output";
 
-app.use(express.json({ limit: 50 * 1024 * 1024 }));
+app.use(express.json({ limit: '1gb' }));
 app.use(cors());
 app.use('/output', express.static(outputDirectory));
 
